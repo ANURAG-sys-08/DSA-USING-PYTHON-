145c2e5b576e060bae7c53107ce60e737dd3c6aa
ANURAG-sys-08 <anuragchauhan0810@gmail.com>
2025-07-18 13:03:56 +0530

    Add files via upload

---
+++ Merge Multiple Dictionaries.py
@@ -0,0 +1,16 @@
+# Merge Multiple Dictionaries
+
+# Design a Python function named merge_three_dictionaries to merge exactly three dictionaries into one.
+
+# Example:
+
+# Input: ({'a': 1, 'b': 2}, {'c': 3, 'd': 4}, {'e': 5, 'f': 6})
+# Output: {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+
+# Input: ({'x': 10, 'y': 20}, {'z': 30}, {'a': 40, 'b': 50})
+# Output: {'x': 10, 'y': 20, 'z': 30, 'a': 40, 'b': 50}
+
+def merge_three_dictionaries(dict1, dict2, dict3):
+    merged_dict = {**dict1,**dict2,**dict3}
+    return merged_dict
+print(merge_three_dictionaries({'a': 1, 'b': 2}, {'c': 3, 'd': 4}, {'e': 5, 'f': 6}))
