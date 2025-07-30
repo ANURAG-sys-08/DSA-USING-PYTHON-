f2cbef0b2f2691d6998ae7bdb8fa14dc585ecbc0
Anurag Chauhan <anuragchauhan0810@gmail.com>
2025-07-30 23:56:09 +0530

    Add files via upload

---
+++ selection sort.py
@@ -0,0 +1,13 @@
+# selection sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1,n):
+            if (arr[j]<arr[min_index]):
+                min_index = j
+        arr[i],arr[min_index] = arr[min_index],arr[i]
+    return arr
+print(selection_sort([11,32,12,43,12,54]))
+
+
